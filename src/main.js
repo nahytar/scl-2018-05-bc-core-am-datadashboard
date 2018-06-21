44c066a1956c8a802b7e461929920a97d2f40487
Mayra Romero <nahytar@gmail.com>
2018-06-20 21:37:15 -0400

    se soluciona bag del select

--- src/main.js
+++ src/main.js
@@ -29,20 +29,17 @@ const toggle = (event) => {
   event.stopPropagation();
 };
 
-for (let i = 0; i < elements.length; i++) {
-  elements[i].addEventListener('click', toggle);
-}
-
 const hola = document.getElementById('hola');
 
 hola.addEventListener('click', () => {
   const x = document.getElementById('select_student');
-  if(x === "none"){
-    x.style.display = "block";
-  }else{
-    x.style.display = "none";
+  if (x === 'none') {
+    x.style.display = 'block';
+  } else {
+    x.style.display = 'none';
   }
-})
+});
+
 const tags = document.getElementsByClassName('tag');
 
 const sitesDiv = document.getElementById('sites');
